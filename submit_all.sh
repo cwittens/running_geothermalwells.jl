c401a169773c7c8c57b49a97bc37084beb78f97a
Collin Wittenstein <cwittens@students.uni-mainz.de>
2026-02-26 12:18:06 -0500

    small change to submit job script

--- submit_all.sh
+++ submit_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Submit all 32 cases to SLURM
 
-for i in $(seq -w 1 8) $(seq -w 13 20) $(seq -w 25 32); do
-    echo "Submitting case_${i}..."
-    sbatch "case_${i}.sh"
+for i in $(seq 1 8) $(seq 13 20) $(seq 25 32); do
+    padded=$(printf "%03d" $i)
+    echo "Submitting case_${padded}..."
+    sbatch "case_${padded}.sh"
 done
 
 echo "All jobs submitted!"
