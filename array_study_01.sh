8957694403cd37958449a2007f8d8cbaa9db27b7
Collin Wittenstein <cwittens@students.uni-mainz.de>
2026-05-08 12:37:28 -0400

    small change to .sh

--- array_study_01.sh
+++ array_study_01.sh
@@ -6,9 +6,9 @@
 #SBATCH -c 4
 #SBATCH -t 6:00:00
 #SBATCH --requeue
-#SBATCH -J study_01
-#SBATCH -o study_01_%j.out
-#SBATCH -e study_01_%j.err
+#SBATCH -J array_study_01
+#SBATCH -o array_study_01_%j.out
+#SBATCH -e array_study_01_%j.err
 
 # Run your simulation
 julia array_study_01.jl
